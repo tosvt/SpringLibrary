ca02c817597740707da72026bc89563b0d83f90f
tosvt <sweb7@ya.ru>
2023-08-30 16:17:30 +0300

    Fix Unit Test

--- src/test/java/org/example/repository/AuthorRepositoryTest.java
+++ src/test/java/org/example/repository/AuthorRepositoryTest.java
@@ -4,11 +4,11 @@ import org.example.domain.Author;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@DataJdbcTest // тестирование отдельно слоя репозитория
+@DataJpaTest // тестирование отдельно слоя репозитория
 class AuthorRepositoryTest {
     @Autowired
     private AuthorRepository authorRepository;
